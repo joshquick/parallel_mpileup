4967b69df1e0be325acf43076d3aacaedb44ed69
joshquick <j.quick@bham.ac.uk>
2013-10-28 12:01:45 +0000

    Added VarScan support

--- parallel_mpileup.py
+++ parallel_mpileup.py
@@ -17,44 +17,58 @@ def which(program):
 				return exe_file
 	return None
 
+def file_len(fname):
+    with open(fname) as f:
+        for i, l in enumerate(f):
+            pass
+    return i + 1
+
 def main(cmdargs):
 	
-	if which('vcfutils.pl'):
-		pass
-	else:
+	if which('vcfutils.pl'):	pass
+	else:	
 		print 'Error: bcftools not found'
 		sys.exit()
+
+	if which('samtools'):	pass
+	else:	
+		print 'Error: samtools not found'
+		sys.exit()
+
+	if which('vcf-concat'):	pass
+	else:	
+		print 'Error: vcftools/PERL5LIB not found'
+		sys.exit()
+
+	if which('VarScan.v2.3.6.jar'): pass
+	else:
+		print 'Error: VarScan.v2.3.6.jar not found'
+		sys.exit()
+
 	args = ['vcfutils.pl', 'splitchr', '-l', cmdargs.chunk, cmdargs.fasta + '.fai']
 	pro = subprocess.Popen(args, stdout=subprocess.PIPE)
 	out, err = pro.communicate()
 	parts = out.split('\n')
 	parts = parts[:-1]
-	
+
 	makefile = open('Makefile', 'w')
 	print >> makefile, 'all:',
 	for each in range(len(parts)):
 		print >> makefile, 'region-%s.vcf' %(each),
 	print >> makefile, '\n'
 	for i, each in enumerate(parts):
-		print i, each
-		print >> makefile, '''region-%s.vcf:
-	samtools mpileup %s %s -r '%s' -b %s | bcftools view %s - > region-%s.vcf
-	''' %(i, cmdargs.mpileup, cmdargs.fasta, each, cmdargs.bamfiles, cmdargs.view, i)
+		if cmdargs.method == 'bcftools':
+			print >> makefile, '''region-%s.vcf:
+	samtools mpileup %s -DSIuf -r '%s' -b %s | bcftools view -cegv - > region-%s.vcf
+	''' %(i, cmdargs.fasta, each, cmdargs.bamfiles, i)
+		if cmdargs.method == 'varscan':
+			print >> makefile, '''region-%s.vcf:
+	samtools mpileup -f %s -r '%s' -b %s | java -jar bin/VarScan.v2.3.6.jar mpileup2snp --min-coverage 2 --min-var-freq 0.9 --p-value 0.005 --output-vcf --vcf-sample-list %s > region-%s.vcf
+	''' %(i, cmdargs.fasta, each, cmdargs.bamfiles, cmdargs.bamfiles, i)
 	makefile.close()
 
-	if which('samtools'):
-		pass
-	else:
-		print 'Error: samtools not found'
-		sys.exit()
 	args = ['make', '-j', cmdargs.threads]
 	subprocess.call(args, stdout=subprocess.PIPE)
-
-	if which('vcf-concat'):
-		pass
-	else:
-		print 'Error: vcftools/PERL5LIB not found'
-		sys.exit()
 	
 	list = []
 	for each in range(len(parts)):
@@ -73,10 +87,9 @@ if __name__ == '__main__':
 	parser = argparse.ArgumentParser(description='This script is designed to speed up samtools mpileup by running a subregion per thread then merging the output.', formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 	parser.add_argument('fasta', help="Reference fasta")
 	parser.add_argument('bamfiles', help="File, list of input BAM files")
+	parser.add_argument('method', choices=['bcftools', 'varscan'], help="Default settings are: 'bcftools view -cegv -' and 'VarScan.v2.3.6.jar mpileup2snp --min-coverage 3 --min-reads2 2 --min-avg-qual 20 --min-var-freq 0.9 --p-value 0.005 --strand-filter 1 --output-vcf 1'")
 	parser.add_argument('-t', '--threads', metavar='', help="Number of threads", default='32')
 	parser.add_argument('-c', '--chunk', metavar='', help="Chunk size in bases", default='250000')
-	parser.add_argument('-m', '--mpileup', metavar='', help="samtools mpileup options", default='-DSIuf')
-	parser.add_argument('-v', '--view', metavar='', help="bcftools view options", default='-cegv')
 	parser.add_argument('-o', '--output', metavar='', help="Output suffix", default='merged')
 	cmdargs = parser.parse_args()
 	main(cmdargs)
